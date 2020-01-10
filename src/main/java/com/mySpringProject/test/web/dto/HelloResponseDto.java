6ea078232a6d032ae8fb96c395092f1120e2a19c
jaehyun han <>
2020-01-10 15:44:56 +0900

    jpa 테스트 코드 작성하기

--- src/main/java/com/mySpringProject/test/web/dto/HelloResponseDto.java
+++ src/main/java/com/mySpringProject/test/web/dto/HelloResponseDto.java
@@ -2,8 +2,8 @@ package com.mySpringProject.test.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
+@Getter // 선언된 모든 필드의 get 메소드를 생성합니다.
+@RequiredArgsConstructor // 선언된 모든 필드가 포함된 생성자를 생성해줍니다. 단 final이 있는 필드만 포함됩니다.
 public class HelloResponseDto {
     private final String name;
     private final int amount;
--- src/test/java/com/mySpringProject/test/HelloControllerTest.java
+++ src/test/java/com/mySpringProject/test/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) //JUnit 과 SpringRunner 스프링 실행자를 실행합니다.
@@ -24,5 +25,17 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // /hello 주소로 get 요청을 합니다.
                 .andExpect(status().isOk()) // mvc.perform 의 결과를 검증합니다. isOk는 get 의 결과로 200을 받았는지 검증합니다.
                 .andExpect(content().string(hello)); // HelloController 에서 hello를 반환하기 때문에 이 값이 맞는지 검증합니다.
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name).param("amount", String.valueOf(amount))) // API테스트할 때 사용될 요청 파라메터를 설정합니다. 값은 String 만 허용됩니다.
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메서드입니다.
+                .andExpect(jsonPath("$.amount", is(amount))); // $를 기준으로 필드명을 명시합니다.
     }
 }
