48d16ecdb1140343bdf6ab8566bf55165404c6c6
jaehyun han <>
2020-01-13 14:29:38 +0900

    jpa auditing 기능 추가

--- src/main/java/com/mySpringProject/test/Application.java
+++ src/main/java/com/mySpringProject/test/Application.java
@@ -1,7 +1,9 @@
 package com.mySpringProject.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // jpa Auditin 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
